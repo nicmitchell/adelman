4b92a9d2bc445ec919a119dcdf48064be801cbe0
Nic Mitchell <nic@nicmitchell.com>
2015-05-31 21:11:35 -0700

    added sold out shortcode

--- functions.php
+++ functions.php
@@ -104,7 +104,7 @@ add_action('init', 'cptui_register_my_cpt_artist');
         'rewrite' => array('slug' => 'artist', 'with_front' => true),
         'query_var' => true,
         'has_archive' => true,
-        'supports' => array('title','editor','excerpt','thumbnail','comments'),
+        'supports' => array('title','editor','thumbnail'),
         'taxonomies' => array('artists'),
         'labels' => array (
           'name' => 'Artists',
@@ -139,7 +139,7 @@ add_action('init', 'cptui_register_my_cpt_jeweler');
       'hierarchical' => false,
       'rewrite' => array('slug' => 'jeweler', 'with_front' => true),
       'query_var' => true,
-      'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes','post-formats'),
+      'supports' => array('title','editor','thumbnail'),
       'labels' => array (
         'name' => 'Jewelers',
         'singular_name' => 'Jeweler',
@@ -535,6 +535,76 @@ function adelman_featured_products_admin_filter_query( $query ) {
 }
 add_filter( 'parse_query', 'adelman_featured_products_admin_filter_query' );
 
+// **********************************************************************// 
+// ! Sold Out Products Order
+// **********************************************************************// 
+
+/**
+   * shortcode function.
+   *
+   * @access public
+   * @return string
+   */
+  function sold_out_products_shortcode( $atts ) {
+    global $woocommerce_loop, $woocommerce, $sold_out_shortcode_used;
+    $sold_out_shortcode_used = true;
+
+    extract( shortcode_atts( array(
+      'per_page'  => '12',
+      'columns'   => '4',
+      'orderby' => 'meta_value_num',
+      'order' => 'desc'
+    ), $atts) );
+
+    $meta_query = array();
+    $meta_query[] = array(
+      'key'     => '_visibility',
+      'value'   => array( 'visible', 'catalog' ),
+      'compare' => 'IN'
+    );
+    $meta_query[] = array(
+      'key'     => '_stock_status',
+      'value'   => 'outofstock',
+      'compare'   => '='
+    );
+
+    $args = array(
+      'post_type' => 'product',
+      'post_status' => 'publish',
+      'ignore_sticky_posts' => 1,
+      'posts_per_page' => $per_page,
+      'orderby' => $orderby,
+      'order' => $order,
+      'meta_key' => 'sold_date',
+      'meta_query' => $meta_query
+    );
+
+    ob_start();
+
+    $products = new WP_Query( $args );
+
+    $woocommerce_loop['columns'] = $columns;
+
+    if ( $products->have_posts() ) : ?>
+
+      <?php woocommerce_product_loop_start(); ?>
+
+        <?php while ( $products->have_posts() ) : $products->the_post(); ?>
+
+          <?php woocommerce_get_template_part( 'content', 'product' ); ?>
+
+        <?php endwhile; // end of the loop. ?>
+
+      <?php woocommerce_product_loop_end(); ?>
+
+    <?php endif;
+
+    wp_reset_postdata();
+
+    return '<div class="woocommerce">' . ob_get_clean() . '</div>';
+  }
+
+add_shortcode( 'sold_out_products', 'sold_out_products_shortcode' );
 
 // **********************************************************************// 
 // ! Add PrettyPhoto 'rel' attribute for lightbox

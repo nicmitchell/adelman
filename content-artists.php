4acd24832a42f77780a830ff597d548aa03023f2
Nic Mitchell <nic@nicmitchell.com>
2017-03-12 00:22:40 -0800

    removed small thumbnails from artists page

--- content-artists.php
+++ content-artists.php
@@ -9,74 +9,30 @@
 	$postId = get_the_ID();
 	$lightbox = 1;//etheme_get_option('blog_lightbox');
 	$postClass = 'blog-post post-grid span4';
-	$width = 500;//etheme_get_option('blog_page_image_width');
-	$height = 500;//etheme_get_option('blog_page_image_height');
-	$crop = etheme_get_option('blog_page_image_cropping');
 	$product = (get_post_meta($postId, 'linked_product')) ? array_shift(get_post_meta($postId, 'linked_product'))[0] : false;
-	$args = apply_filters('woocommerce_related_products_args', array(
-		'relation' => 'AND',
-		'post_type'				=> 'product',
-		'ignore_sticky_posts'	=> 1,
-		'no_found_rows' 		=> 1,
-		'numberposts' 	    => 4,
-		'orderby'          => 'post_date',
-		'order'            => 'DESC',
-		'post_status'      => 'publish',
-		'meta_query' => array(
-			array(
-				'key' => 'provider',
-				'value' => '"' . $postId . '"', 
-				'compare' => 'LIKE'
-			)
-		)
-	));
-	$products = get_posts( $args );
 ?>
-
 
 <article <?php post_class($postClass); ?> id="post-<?php the_ID(); ?>" >
 	<header class="artist-header">
 		<h4 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-		<!-- <span class="posted-on">Artwork shown: </span> -->
-		<span class="product-link" style="display:none"><a href="<?php echo get_permalink($product); ?>"><?php echo get_the_title($product); ?></a></span>
+		<span class="product-link" style="display:none">
+			<a href="<?php echo get_permalink($product); ?>"><?php echo get_the_title($product); ?></a>
+		</span>
 	</header>
 
-	<?php $image = etheme_get_image(false, $width,$height,$crop); ?>
-
 	<?php if (has_post_thumbnail()): ?>
+		<?php $image = get_the_post_thumbnail_url($postId); ?>
 		<div class="post-images main-image">
-			<a href="<?php the_permalink(); ?>"><img src="<?php echo $image; ?>"></a>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_post_thumbnail(); ?>
+			</a>
 			<div class="blog-mask">
 				<div class="mask-content">
-					<?php if($lightbox): ?><a href="<?php echo etheme_get_image(get_post_thumbnail_id($postId)); ?>" rel="lightbox"><i class="icon-resize-full"></i></a><?php endif; ?>
+					<?php if($lightbox): ?><a href="<?php the_post_thumbnail_url(); ?>" rel="lightbox"><i class="icon-resize-full"></i></a><?php endif; ?>
 					<a href="<?php the_permalink(); ?>"><i class="icon-link"></i></a>
 				</div>
 			</div>
 		</div>	
-	<?php endif ?>	
-	<div class="post-information <?php if (!has_post_thumbnail()): ?>border-top<?php endif ?>">
-		
-		<div class="post-info">
-			<div class="artist-thumbnails">
-				<?php foreach ( $products as $product ) : ?>
-					<figure class="artist-thumbnail">
-						<a href="<?php echo get_permalink($product->ID); ?>">
-							<?php 
-								$thumb_atts = array(	
-									'alt'	=> trim( strip_tags( $product->post_excerpt ) ),
-									'title'	=> trim( strip_tags( $product->post_title ) )
-								); 
-								echo get_the_post_thumbnail( $product->ID, 'thumbnail', $thumb_atts); ?>
-						</a>
-					</figure>
-				<?php endforeach; ?>
-			</div>
-		</div>
-
-		<div class="post-description"></div>
-
-		<div class="clear"></div>
-		
-	</div>
+	<?php endif ?>
 
 </article>

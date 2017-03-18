9420fc807aa4f1157fea2c813cb618e2230c09c8
Nic Mitchell <nic@nicmitchell.com>
2017-03-17 18:47:50 -0700

    removed icons and link directly to artist’s page on /artist archive

--- archive-artist.php
+++ archive-artist.php
@@ -15,8 +15,7 @@
   <div class="container">
     <div class="row-fluid">
       <div class="span12 a-center">
-        <h1 class="title"><span><?php echo 'Artists'; ?></span></h1>
-        <?php //etheme_breadcrumbs(); ?>
+        <h1 class="title"><span>Artists</span></h1>
       </div>
     </div>
   </div>
@@ -30,31 +29,16 @@
           <?php etheme_get_sidebar($sidebarname); ?>
         </div>
       <?php endif; ?>
-
+      
       <div class="content <?php echo $content_span; ?>">
         <div class="blog-masonry row">
-
           <?php if(have_posts()): while(have_posts()) : the_post(); ?>
-
               <?php get_template_part('content', 'artists'); ?>
-
           <?php endwhile; ?>
-
         </div>
-
-
         <?php else: ?>
-
           <h1><?php _e('No posts were found!', ETHEME_DOMAIN) ?></h1>
-
         <?php endif; ?>
-
-        <div class="articles-nav">
-          <div class="left"><?php next_posts_link(__('&larr; Older Posts', ETHEME_DOMAIN)); ?></div>
-          <div class="right"><?php previous_posts_link(__('Newer Posts &rarr;', ETHEME_DOMAIN)); ?></div>
-          <div class="clear"></div>
-        </div>
-
       </div>
 
       <?php if($position == 'right' || ($responsive == 'bottom' && $position == 'left')): ?>
@@ -67,19 +51,7 @@
 
   </div>
 </div>
-<script>
-jQuery('document').ready(function(){
-  jQuery('.icon-resize-full').on('click', function(){
-    var node = jQuery(this).closest('header.artist-header').next('.post-information');
-    var product = jQuery(this).closest('.post-images').prev('header.artist-header').find('.product-link').html();
-    var artist = ' by ' + jQuery(this).closest('.post-images').prev('header.artist-header').find('.post-title').html();
-    var caption = product + artist;
-    setTimeout(function(){ jQuery('.mfp-title').html(caption).fadeIn(); }, 500);
-  });
-});
-</script>
 
-  
 <?php
   get_footer();
 ?>
--- content-artists.php
+++ content-artists.php
@@ -5,34 +5,18 @@
 
 ?>
 
-<?php 
-	$postId = get_the_ID();
-	$lightbox = 1;//etheme_get_option('blog_lightbox');
-	$postClass = 'blog-post post-grid span4';
-	$product = (get_post_meta($postId, 'linked_product')) ? array_shift(get_post_meta($postId, 'linked_product'))[0] : false;
-?>
-
-<article <?php post_class($postClass); ?> id="post-<?php the_ID(); ?>" >
-	<header class="artist-header">
-		<h4 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-		<span class="product-link" style="display:none">
-			<a href="<?php echo get_permalink($product); ?>"><?php echo get_the_title($product); ?></a>
-		</span>
-	</header>
-
-	<?php if (has_post_thumbnail()): ?>
-		<?php $image = get_the_post_thumbnail_url($postId); ?>
+<?php if (has_post_thumbnail()): ?>
+	<?php $postClass = 'blog-post post-grid span4'; ?>
+	<article <?php post_class($postClass); ?> id="post-<?php the_ID(); ?>" >
+		<header class="artist-header">
+			<h4 class="post-title">
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			</h4>
+		</header>
 		<div class="post-images main-image">
 			<a href="<?php the_permalink(); ?>">
 				<?php the_post_thumbnail(); ?>
 			</a>
-			<div class="blog-mask">
-				<div class="mask-content">
-					<?php if($lightbox): ?><a href="<?php the_post_thumbnail_url(); ?>" rel="lightbox"><i class="icon-resize-full"></i></a><?php endif; ?>
-					<a href="<?php the_permalink(); ?>"><i class="icon-link"></i></a>
-				</div>
-			</div>
 		</div>	
-	<?php endif ?>
-
-</article>
+	</article>
+<?php endif ?>

3ebd787b6a2a1c107c4fc150fa2904878934585b
Nic Mitchell <nic@nicmitchell.com>
2017-07-15 00:12:58 -0700

    updated for v3.0.0, added notes for using hooks

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -9,11 +9,16 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 <?php
 	/**
+	Why we need this custom template:
+	- Reserved items rely on ACF fields
+	- Showing dimensions
+	- Note: Can probalby use hooks to eliminate need for custom templates
+
 	 * Single Product Content
 	 *
 	 * @author 		WooThemes
 	 * @package 	WooCommerce/Templates
-	 * @version     2.1.2
+	 * @version     3.0.0
 	 */
 	/**
 	 * woocommerce_before_single_product hook
@@ -23,7 +28,9 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	 do_action( 'woocommerce_before_single_product' );
 ?>
 
-<div itemscope itemtype="http://schema.org/Product" id="product-<?php the_ID(); ?>" <?php post_class('single-product-page'); ?>>
+<?php $tabs_position = ( etheme_get_option( 'tabs_position' ) == 'tabs-under' ) ? 'under' : 'inside'; ?>
+
+<div id="product-<?php the_ID(); ?>" <?php post_class('single-product-page'); ?>>
 	
 	<div class="row product-info sidebar-position-<?php echo $position; ?> responsive-sidebar-<?php echo $responsive; ?>">
 		<?php
@@ -35,92 +42,90 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 			 */
 			do_action( 'woocommerce_before_single_product_summary' );
 		?>
-
-		<?php if($single_product_sidebar && ($position == 'left' || ($responsive == 'top' && $position == 'right'))): ?>
-			<div class="span3 sidebar sidebar-left single-product-sidebar">
-				<?php et_product_brand_image(); ?>
-				<?php if(etheme_get_option('upsell_location') == 'sidebar') woocommerce_upsell_display(); ?>
-				<?php dynamic_sidebar('single-sidebar'); ?>
+		<div class="content-area <?php echo ( $position == 'left' ) ? 'pull-right' : '' ; ?>">
+			<div class="span<?php echo $images_span; ?>">
+				<?php woocommerce_show_product_images(); ?>
 			</div>
-		<?php endif; ?>
-
-		<div class="span<?php echo $images_span; ?>">
-			<?php woocommerce_show_product_images(); ?>
-		</div>
-		<div class="span<?php echo $meta_span; ?> product_meta">
-			<?php if (etheme_get_option('show_name_on_single')): ?>
-				<h2 class="product-name"><?php the_title(); ?></h2>
-			<?php endif; ?>
-			<div style="display:none;" itemprop="name"><?php the_title(); ?></div>
-			
-			<h4 style="display:none;"><?php _e('Product Information', ETHEME_DOMAIN) ?></h4>
-			
-			<?php woocommerce_template_loop_rating(); ?>
-			
-			<span class="artist_name_wrapper"><?php adelman_product_artist(); ?></span>
-			
-			<?php $version_short = $product->get_attribute( 'pa_version-short' ); ?>
-			<?php if($version_short): ?>
-				<span class="short_description_meta">Version: <?php echo $version_short; ?></span>
-			<?php endif; ?>
-
-			<?php $dimensions = $product->get_dimensions(false); ?>
-			<?php if ($dimensions): ?>
-				<span class="short_description_meta">Dimensions: <?php echo wc_format_dimensions($dimensions); ?></span>
-			<?php endif; ?>
-			
-			<?php
-				$size = sizeof( get_the_terms( $post->ID, 'product_cat' ) );
-				echo wc_get_product_category_list( ', ', '<span class="short_description_meta">' . _n( 'Category:', 'Categories:', $size, 'woocommerce' ) . ' ', '</span>' );
-			?>
-			
-			<?php
-				/**
-				 * woocommerce_single_product_summary hook
-				 *
-				 * @hooked woocommerce_template_single_price - 10
-				 * @hooked woocommerce_template_single_excerpt - 20
-				 */
-				do_action( 'woocommerce_single_product_summary' );
-			?>
-			
-		    <?php if ( etheme_get_custom_field('size_guide_img') ) : ?>
-		    	<?php $lightbox_rel = (get_option('woocommerce_enable_lightbox') == 'yes') ? 'prettyPhoto' : 'lightbox'; ?>
-		        <div class="size_guide">
-		    	 <a rel="<?php echo $lightbox_rel; ?>" href="<?php etheme_custom_field('size_guide_img'); ?>"><?php _e('SIZING GUIDE', ETHEME_DOMAIN); ?></a>
-		        </div>
-		    <?php endif; ?>	
-			<?php $reserved = get_field('reserved'); ?>
-			<?php if (!$reserved): ?>
-			<?php woocommerce_template_single_add_to_cart(); ?>
-			<?php endif; ?>
-
-			<?php woocommerce_template_single_meta(); ?>
-			<?php if($reserved): ?>
-			<h4>This item is reserved</h4>
-			<?php endif; ?>
-            
-            <?php if(etheme_get_option('share_icons')): ?>
-							<span class="share-button-title">Share</span>
-							<?php echo do_shortcode('[share text="'.get_the_title().'"]'); ?>
-						<?php endif; ?>
-            
-			<?php woocommerce_template_single_sharing(); ?>
+			<div class="span<?php echo $meta_span; ?> product_meta">
+				<?php if (etheme_get_option('show_name_on_single')): ?>
+					<h2 class="product-name"><?php the_title(); ?></h2>
+				<?php else : ?>
+					<div style="display:none;" itemprop="name" class="product-name-hiden"><?php the_title(); ?></div>
+				<?php endif; ?>
+				<h4><?php _e('Product Information', ETHEME_DOMAIN) ?></h4>
 				
-		</div>
+				<?php woocommerce_template_single_rating(); ?>
+				<?php /* May be able to hook into woocommerce_template_single_rating */ ?>
+				<span class="artist_name_wrapper"><?php adelman_product_artist(); ?></span>
+				
+				<?php $version_short = $product->get_attribute( 'pa_version-short' ); ?>
+				<?php if($version_short): ?>
+					<span class="short_description_meta">Version: <?php echo $version_short; ?></span>
+				<?php endif; ?>
 
-		<?php if($single_product_sidebar && ($position == 'right' || ($responsive == 'bottom' && $position == 'left'))): ?>
-			<div class="span3 sidebar sidebar-right single-product-sidebar">
+				<?php $dimensions = $product->get_dimensions(false); ?>
+				<?php if ($dimensions): ?>
+					<span class="short_description_meta">Dimensions: <?php echo wc_format_dimensions($dimensions); ?></span>
+				<?php endif; ?>
+
+				<?php if ( $product->is_type( array( 'simple', 'variable' ) ) && $product->get_sku() ) : ?>
+					<span itemprop="productID" class="sku_wrapper"><?php _e( 'Product code', ETHEME_DOMAIN ); ?>: <span class="sku"><?php echo $product->get_sku(); ?></span></span>
+				<?php endif; ?>
+
+				<?php echo wc_get_product_category_list( $product->get_id(), ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', count( $product->get_category_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
+				
+				<?php
+					/**
+					 * woocommerce_single_product_summary hook
+					 *
+					 * @hooked woocommerce_template_single_price - 10
+					 * @hooked woocommerce_template_single_excerpt - 20
+					 */
+					do_action( 'woocommerce_single_product_summary' );
+				?>
+				
+					<?php if ( etheme_get_custom_field('size_guide_img') ) : ?>
+						<?php $lightbox_rel = 'lightbox'; ?>
+							<div class="size_guide">
+						 <a rel="<?php echo $lightbox_rel; ?>" href="<?php etheme_custom_field('size_guide_img'); ?>"><?php _e('SIZING GUIDE', ETHEME_DOMAIN); ?></a>
+							</div>
+					<?php endif; ?>	
+					
+					<?php /* may be able to hook reserved stuff into woocommerce_single_product_summary */ ?>
+						<?php $reserved = get_field('reserved'); ?>
+						<?php if (!$reserved): ?>
+							<?php woocommerce_template_single_add_to_cart(); ?>
+						<?php endif; ?>
+					 
+						<?php woocommerce_template_single_meta(); ?>
+						<?php if($reserved): ?>
+							<h4>This item is reserved</h4>
+						<?php endif; ?>
+							
+						<?php if(etheme_get_option('share_icons')) echo do_shortcode('[share text="'.get_the_title().'"]'); ?>
+							
+				<?php woocommerce_template_single_sharing(); ?>
+					
+			</div>
+			
+			<?php if ( $tabs_position == 'inside' ) : ?>
+				<div class="tabs-under-product">
+					<?php woocommerce_output_product_data_tabs(); ?>
+				</div>
+			<?php endif; ?>
+		</div>
+			<div class="span3 sidebar sidebar-<?php echo $position; ?> pull-<?php echo $position; ?> mobile-sidebar-<?php echo $responsive; ?> single-product-sidebar">
 				<?php et_product_brand_image(); ?>
 				<?php if(etheme_get_option('upsell_location') == 'sidebar') woocommerce_upsell_display(); ?>
 				<?php dynamic_sidebar('single-sidebar'); ?>
 			</div>
-		<?php endif; ?>
-
 	</div>
 	
 	<?php
-		woocommerce_output_product_data_tabs();
+	
+		if ( $tabs_position != 'inside' ) {
+			woocommerce_output_product_data_tabs();
+		}
 
 		if(etheme_get_custom_field('additional_block') != '') {
 			echo '<div class="sidebar-position-without">';
@@ -128,10 +133,12 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 			echo '</div>';
 		} 
 
-	  	if(etheme_get_option('upsell_location') == 'after_content') woocommerce_upsell_display();
-	  	if(etheme_get_option('show_related'))
+			if(etheme_get_option('upsell_location') == 'after_content') woocommerce_upsell_display();
+			if(etheme_get_option('show_related'))
 			woocommerce_output_related_products();
 	?>
+
+	<meta itemprop="url" content="<?php the_permalink(); ?>" />
 
 </div><!-- #product-<?php the_ID(); ?> -->
 
